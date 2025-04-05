bc98b38fa138d8f76fe6c85bfe383e1c7e0c06a9
Ethan Hall <ethanwhall@icloud.com>
2025-04-05 07:13:00 -0400

    Create express server, file structure

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const path = require("path");
+const passport = require("passport");
+const session = require("express-session");
+
+//ROUTER IMPORTS
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath));
+
+app.use(express.urlencoded({ extended: true }));
+
+//ROUTES
+//404 route
+app.use((req, res) => res.status(404).render("pages/404"));
+
+app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
